cca929c3b6be80a9414134fe88b0a8441345c7af
christina ly <christina ly>
2016-08-23 16:51:24 -0400

    jQuery_group_assignment

---
+++ Christina Ly/spaceneedle.js
@@ -0,0 +1,11 @@
+for(var i=0; i<4; i++){
+	console.log("          ||");
+}
+console.log("       __/||\\__");
+var colon= ":"
+for(var i=0;i<4; i++){
+	for(var k=0; k<11; k++){
+		var row=" ";
+	}
+}
+console.log(row);
